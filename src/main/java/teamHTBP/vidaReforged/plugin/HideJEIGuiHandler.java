015f25eeb48fdeedc552ec2f18b9b5d639a0e547
DespairP <328060206@qq.com>
2023-08-01 02:40:32 +0800

    add jei support

---
+++ src/main/java/teamHTBP/vidaReforged/plugin/HideJEIGuiHandler.java
@@ -0,0 +1,22 @@
+package teamHTBP.vidaReforged.plugin;
+
+import com.google.common.collect.ImmutableList;
+import mezz.jei.api.gui.handlers.IGuiContainerHandler;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.renderer.Rect2i;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class HideJEIGuiHandler<T extends AbstractContainerScreen<?>> implements IGuiContainerHandler<T> {
+
+    @Override
+    public @NotNull List<Rect2i> getGuiExtraAreas(@NotNull T containerScreen) {
+        return ImmutableList.of(new Rect2i(0,0,containerScreen.width, containerScreen.height));
+    }
+
+    public static <T extends AbstractContainerScreen<?>> HideJEIGuiHandler<T> create(Class<T> clazz){
+        return new HideJEIGuiHandler<>();
+    }
+
+}
---
+++ src/main/java/teamHTBP/vidaReforged/plugin/VidaJeiPlugin.java
@@ -0,0 +1,30 @@
+package teamHTBP.vidaReforged.plugin;
+
+
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.helpers.IPlatformFluidHelper;
+import mezz.jei.api.registration.*;
+import mezz.jei.api.runtime.IJeiRuntime;
+import mezz.jei.api.runtime.config.IJeiConfigManager;
+import net.minecraft.resources.ResourceLocation;
+import teamHTBP.vidaReforged.client.screen.MagicWordCraftingTableScreen;
+import teamHTBP.vidaReforged.client.screen.MagicWordScreen;
+import teamHTBP.vidaReforged.core.common.system.guidebook.TeaconGuideBook;
+
+import static teamHTBP.vidaReforged.VidaReforged.MOD_ID;
+
+@JeiPlugin
+public class VidaJeiPlugin implements IModPlugin {
+    @Override
+    public ResourceLocation getPluginUid() {
+        return new ResourceLocation(MOD_ID, "jei_plugin");
+    }
+
+    @Override
+    public void registerGuiHandlers(IGuiHandlerRegistration registration) {
+        registration.addGuiContainerHandler(MagicWordScreen.class, HideJEIGuiHandler.create(MagicWordScreen.class));
+        registration.addGuiContainerHandler(MagicWordCraftingTableScreen.class, HideJEIGuiHandler.create(MagicWordCraftingTableScreen.class));
+        registration.addGlobalGuiHandler(new HideJEIScreenHandler());
+    }
+}
